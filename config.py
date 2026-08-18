7f9bd295a5c775c68d22a196f008cbb2129af5a3
Ali-dev25 <alidev2oo25@gmail.com>
2026-08-18 18:05:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# البيانات الأساسية (يتم جلبها من البيئة أو وضعها مباشرة)
-BOT_TOKEN = os.getenv("BOT_TOKEN","8885795921:AAHomJzVOEVfW1ddhlSC6Et69NL8X9Td5kM")
-API_ID = int(os.getenv("API_ID", "39360245"))
-API_HASH = os.getenv("API_HASH", "3703ecea7a48f62c1f1fa5ee97d5c9e1")
+# توكن البوت والآيدي الخاص بك (الأدمن)
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8885795921:AAHomJzVOEVfW1ddhlSC6Et69NL8X9Td5kM")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "5739511727"))
 
-# بيانات التحويل المحلي
+# بيانات التحويل المحلي والاشتراكات
 STC_PAY_NUMBER = "0500000000"
 BANK_IBAN = "SA0000000000000000000000"
 PRICE_SAR = "120 ريال"
